cd6781440f8b01cd7a850acfd5913a79a423a2cd
Jefferson Carlos Lima <92160349+JeffersonCarlosLima@users.noreply.github.com>
2022-04-19 08:43:39 -0300

    Modelo de lista com filtro em JavaScript

---
+++ index.md
@@ -0,0 +1,4 @@
+Criar botão para o trailler
+    vermelhor com icone de vídeo
+
+Colocar icone nos botões de download
---
+++ js/index.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var input, filter, ul, li, a, i, p, txtValue;
+    input = document.getElementById("PesquisaFilmes");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("ListaFilmes");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
